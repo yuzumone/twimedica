715d49ed272b5d78ec7536cd52eb0bf23ffb5ba1
yuzumone <yuzumone@outlook.com>
2017-04-12 11:06:15 +0900

    Enable to work

--- app/src/main/java/net/yuzumone/twimedica/activity/MainActivity.java
+++ app/src/main/java/net/yuzumone/twimedica/activity/MainActivity.java
@@ -1,15 +1,59 @@
 package net.yuzumone.twimedica.activity;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.twitter.sdk.android.tweetui.SearchTimeline;
+import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 
 import net.yuzumone.twimedica.R;
+import net.yuzumone.twimedica.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    private ActivityMainBinding binding;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        MenuItem item = menu.findItem(R.id.menu_search);
+        searchView = (SearchView) MenuItemCompat.getActionView(item);
+        if (searchView != null) {
+            searchView.setMaxWidth(Integer.MAX_VALUE);
+            searchView.setOnQueryTextListener(MainActivity.this);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        searchView.clearFocus();
+        SearchTimeline timeline = new SearchTimeline.Builder()
+                .query(query)
+                .build();
+        TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
+                .setTimeline(timeline)
+                .build();
+        binding.list.setAdapter(adapter);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
